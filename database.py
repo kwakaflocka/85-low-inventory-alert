fe8772456ccc6d1286c3da72988496ddbcfe406e
kwakaflocka <99681933+kwakaflocka@users.noreply.github.com>
2022-09-07 12:16:32 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,139 +1,66 @@
-from datetime import datetime, timedelta
-import pyodbc
 import pandas as pd
-from pandasql import sqldf
+from mssql_dataframe import SQLServer
 
-server = 'DESKTOP\SQLEXPRESS'
+import main
+import sendsms
+
+server = 'LOCALHOST\SQLEXPRESS'
 database = 'NewInventoryDb'
+username='sa'
+password='123456'
+sql = SQLServer(database=database, server=server, username=username, password=password, driver='ODBC Driver 17 for SQL Server')
+data=""
+Yesterday=main.yesterday
+#import AllItemsReport csv into a dataframe
+def csvtodf():
+    path=fr'C:\Users\sabri\Desktop\Toast\{Yesterday}\AllItemsReport.csv'
+    df=pd.read_csv(path)
+    print(df)
+    return df
+
+def loadsqlfromcsv():
+    dailyreports_df = csvtodf()
+    #add date column to data frame
+    dailyreports_df['Date'] = Yesterday
+    #delete yesterday's dataloadfromcsv table
+    try:
+        cursor = sql.connection.cursor()
+        sqlstr='exec spDel_dataloadfromcsv'
+        cursor.execute(sqlstr)
+        print(f"Deleted {Yesterday} dataloadfromcsv")
+    except:
+        print(f'Try: delete {Yesterday} dataloadfromcsv but did not exist. Creating new dataloadfromcsv')
+        pass
+    print(f"Creating 'dataloadfromcsv' SQL table with {Yesterday}")
+
+#dataloadfromcsv is a temporary table and is inserted into history_dataloadfromcsv to keep an archive
+#of report information
+    sql.create.table_from_dataframe(table_name='dataloadfromcsv', dataframe=dailyreports_df)
+    print(dailyreports_df)
+    sql.write.insert(table_name='history_dataloadfromcsv', dataframe=dailyreports_df)
+
+#execute the stored procedures to perform the calculations and update/check the inventory
+def sqlstoredprocedure(sp):
+    cursor=sql.connection.cursor()
+    sqlstr=f'exec {sp}'
+    cursor.execute(sqlstr)
+    if sp == 'spCheckMin': #this calls sendsms.py to send a text alerting what items need to be ordered
+        data=cursor.fetchval()
+        if data:
+            print(f"{data}: Order more")
+            sendsms.inventorySMS(data)
+            return data
+        else:
+            print("Inventory quantities sufficient. No ordering necessary")
 
 
-DB = {'servername': server,
-      'database': database}# create the connection
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + DB['servername'] + ';DATABASE=' + DB['database'] + ';Trusted_Connection=yes')
+loadsqlfromcsv()
 
+#Execute stored procedures
+#'ReportItemsUsed' uses the Toast sales report and inventory quantities in the database to calculate quantity of ingredients used according to each report
+#'spUpdateIngredient' updates the inventory quantities by subtracting quantities from 'ReportItemsUsed'
+#'spCheckMin' checks current inventory quantity against minimum threshold
+sqlstoredprocedure('ReportItemsUsed')
+sqlstoredprocedure('spUpdateIngredient')
+sqlstoredprocedure('spCheckMin')
 
-#conn=pyodbc.connect(f'DRIVER={SQL Server};SERVER='{SQLserver}';DATABASE='{database}';Trusted_Connection=yes')
-#HIDE password
-
-def Yesterday(frmt='%Y-%m-%d', string=True):
-    yesterday=datetime.now()-timedelta(1)
-    if string:
-        return yesterday.strftime(frmt)
-    return yesterday
-## convert to YYYYMMDD
-yesterday=Yesterday()
-def remove_hyphens(string):
-    string=string.replace('-', '')
-    return string
-yesterday=remove_hyphens(yesterday)
-
-#either set date=yesterday or provide a date in YYYYMMDD if you have a specific date you want to download. Toast stores up to 3 weeks worth of files
-date=yesterday
-
-#
-#
-# if cnxn:
-#     print('True')
-# cursor = cnxn.cursor()
-#
-# def Read(cnxn):
-#     table_name=str(input("What table do you want to read from?"))
-#     print('Read')
-#     cursor.execute(f'SELECT * FROM {table_name}')
-#     for row in cursor:
-#         print(row)
-#     cnxn.commit()
-# #TEST Read
-# #Read(cnxn)
-
-
-# def Insert(cnxn):
-#     print('Write')
-#     table_name=input('What table do you want to write to?')
-#     column_name=input('What column do you want to write to?')
-#     entry= input('What do you want to insert?')
-#     insert_stmt = (
-#         f"INSERT INTO {table_name} ({column_name}) VALUES ({entry})"
-#     )
-#     cursor.execute(insert_stmt)
-#
-#     #preview insert statement
-#     print('Read')
-#     cursor.execute(f'SELECT * FROM {table_name}')
-#     for row in cursor:
-#         print(row)
-#
-#     cnxn.commit()
-
-
-#SQL statement to update Ingredient table with new QuantityAvailable with # Orders from AllItemsReport (downloaded from Toast AWS bucket)
-sqlUpdateIngredient= """("update Ingredient"
-      "set"
-      "QuantityAvailable = ISNULL(QuantityAvailable - (select sum(mii.Quantity * a.[  # Orders]) as QtyOfIngUsed)"
-      "from MenuItem m"
-      "inner join AllItemsReport1 a"
-      "on m.Name = a.[Menu Item]"
-      "inner join MenuItemIngredient mii"
-      "on mii.MenuItemId = m.MenuItemId"
-      "inner join Ingredient i"
-      "on mii.IngredientId = i.IngredientId"
-      "where i.IngredientId = ix.IngredientId"
-      "group by m.MenuItemId, m.Name, i.Name, mii.IngredientId), ix.QuantityAvailable)"
-      "from Ingredient ix")
-"""
-
-#Created DailyReport table in NewInventoryDb to be updated daily:
-# cursor.execute('''
-# 		CREATE TABLE DailyReport (
-# 			MasterID int primary key,
-# 			MenuItem nvarchar(50),
-# 			ItemQty int
-# 			)
-#                ''')
-# cnxn.commit()
-
-# 2) Import AllItemsReport.csv into a DataFrame TODO:change path to Toast\{date}\AllItemsReport after everything else works
-# data= pd.read_csv(rf'C:\Users\sabri\Desktop\Toast\20220801\AllItemsReport.csv')
-#
-# df=pd.DataFrame(data)
-# df.columns = df.columns.str.replace(' ','')
-# df = df.fillna(value=0)
-# print("Print df:")
-# print(df)
-# 3) Create a df Table for AllItemsReport.csv with columns Master ID, Menu Item, Item Qty
-select = """SELECT MenuItem, SUM([ItemQty]) as ItemQty
-FROM df
-GROUP BY MenuItem;"""
-
-
-
-report = pysqldf(select)
-report=report.fillna(value=0)
-# print({date})
-print(report)
-
-
-# 4) Insert DataFrame Data into the Table TODO: how to handle duplicate MenuItem records
-
-#Start here: Fix Error for sqlInsert: Violation of PRIMARY KEY constraint 'PK__AllItems__F6B782C4E21BCAD4'. Cannot insert duplicate key in object 'dbo.DailyReport'. The duplicate key value is (HH Buffalo Wings).
-#https://www.tutorialspoint.com/sql/sql-check.htm 1) Use constraints to avoid duplicate insertion and 2) if duplicate, add ItemQty to existing column
-sqlInsert="""
-INSERT INTO [DailyReport] (MenuItem,ItemQty) values(?,?)
-"""
-
-testsql="""
-    INSERT INTO DailyReport (MenuItem,ItemQty) values(?,?) 
-"""
-
-df = pd.read_sql_query(testsql,conn)
-# for index,row in report.iterrows():
-#     newVals = row['MenuItem'], row['ItemQty']
-#
-#     cursor.execute(testsql,newVals)
-
-#df.to_sql('AllItemsReport', con=cnxn, if_exists='replace')
-
-
-cnxn.execute(testsql)
-cnxn.commit()
-cursor.close()
